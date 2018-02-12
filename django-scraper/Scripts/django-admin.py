b8a20aec07ee37ad1bc46120a154d65284a2b103
shrik450 <shrik450@gmail.com>
2018-02-12 20:02:53 +0530

    Removed virtual environment.

--- django-scraper/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\shrikanth upadhayaya\desktop\academics\btp\django scraper\django-scraper\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
